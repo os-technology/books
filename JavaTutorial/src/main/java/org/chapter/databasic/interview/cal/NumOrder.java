08372c5bf7843016ac95fec36b4617a54deb6274
shuijiayou <602570224@qq.com>
2016-06-18 13:49:48 +0800

    1

--- JavaTutorial/src/main/java/org/chapter/databasic/interview/cal/NumOrder.java
+++ JavaTutorial/src/main/java/org/chapter/databasic/interview/cal/NumOrder.java
@@ -21,33 +21,11 @@ public class NumOrder {
 		System.out.println(out.length());
 	}
 
-	// 5，13，1。排序最大值，5.11.1。
-	// 5,13,14,结果：5-14-13
-	// 5,122,13,结果：5-13-122
-	private void getNumOrder() {
-		int[] a = { 1, 5, 21 };
-		for (int i = 0; i < a.length; i++) {
-			for (int j = 0; j < a.length - i - 1; j++) {
-				if (getFirstNum(a[j]) < getFirstNum(a[j + 1])) {
-					int tmp = a[j];
-					a[j] = a[j + 1];
-					a[j + 1] = tmp;
-				}
-			}
-		}
-		for (int out : a) {
-			System.out.print(out + "-");
-		}
-
-	}
-
-	private int getFirstNum(int input) {
-		String in = String.valueOf(input);
-		char inp = in.charAt(0);
-		return Integer.parseInt(inp + "");
-	}
-
 	/**
+	 * 5，13，1。排序最大值，5.11.1。<br>
+	 * 5,13,14,结果：5-14-13<br>
+	 * 5,122,13,结果：5-13-122<br>
+	 * 5,56,54,55结果：56-55-5-54<br>
 	 * 
 	 * @param input
 	 *            输入值
@@ -57,12 +35,15 @@ public class NumOrder {
 	 */
 	private int getPointNum(int input, int len) {
 		String in = String.valueOf(input);
-		if(in.length()<len) return 9999;
-		String inp = in.substring(len-1, len);
+		if (in.length() < len)
+			return 9999;
+		String inp = in.substring(len - 1, len);
 		return Integer.parseInt(inp + "");
 	}
 
 	int count = 1;
+	int lastRes = 0;
+	int thisRes = 0;
 
 	private void getNumOrder(int[] a, int max) {
 		for (int i = 0; i < a.length; i++) {
@@ -75,6 +56,15 @@ public class NumOrder {
 			}
 		}
 		if (count < max) {
+//			if (count == 1) {
+//				lastRes = getPointNum(a[0],count);
+//				thisRes = getPointNum(a[0],count);
+//			} else {
+//				lastRes = thisRes;
+//				thisRes = a[0];
+//			}
+			
+			
 			getNumOrder(a, count++);
 		} else {
 			for (int out : a) {
@@ -87,9 +77,8 @@ public class NumOrder {
 	public static void main(String[] args) {
 		NumOrder num = new NumOrder();
 		// num.zeroCount();
-		// num.getNumOrder();
-		int[] a = { 132, 5, 14 ,15};// 5,14,13
-		num.getNumOrder(a, 2);
+		int[] a = { 5, 54, 543, 56, 4 };// 5,14,13
+		num.getNumOrder(a, 3);
 
 	}
 
