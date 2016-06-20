e952ab6d3f183c4fcaccc7c847bb32273ea08480
shuijiayou <602570224@qq.com>
2016-06-20 14:37:44 +0800

    复习设计模式+java8基础学习

---
+++ JavaTutorial/src/main/java/org/chapter/databasic/eight/ChildExtend.java
@@ -0,0 +1,12 @@
+package org.chapter.databasic.eight;
+
+public class ChildExtend extends Extend {
+	public ChildExtend(){
+		
+	}
+	 
+	public static void main(String[] args) {
+		ChildExtend e = new ChildExtend();
+		e.show();
+	}
+}
---
+++ JavaTutorial/src/main/java/org/chapter/databasic/eight/Extend.java
@@ -0,0 +1,10 @@
+package org.chapter.databasic.eight;
+
+public class Extend {
+public Extend(){
+	
+}
+public void show(){
+	System.out.println("class A");
+}
+}
