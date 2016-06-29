f241b034af2e18156c6240f8ec1be954917bd334
shuijiayou <602570224@qq.com>
2016-06-29 21:03:33 +0800

    多线程回调

---
+++ JavaTutorial/src/main/java/org/chapter/databasic/thread/template/MyCallable.java
@@ -0,0 +1,50 @@
+package org.chapter.databasic.thread.template;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+/**
+ * {@link http://www.journaldev.com/1090/java-callable-future-example}
+ * 
+ * @author gao
+ *
+ */
+public class MyCallable implements Callable<String> {
+
+	@Override
+	public String call() throws Exception {
+		Thread.sleep(1000);
+		return Thread.currentThread().getName();
+	}
+
+	public static void main(String[] args) {
+		// Get ExecutorService from Executors utility class,thread pool size is
+		// 10
+		ExecutorService executor = Executors.newFixedThreadPool(10);
+		// create a list to hold the Future object associated with Callable
+		List<Future<String>> list = new ArrayList<Future<String>>();
+		// Create MyCallable instance
+		Callable<String> callable = new MyCallable();
+		for (int i = 0; i < 100; i++) {
+			// submit Callable tasks to be executed by thread pool
+			Future<String> future = executor.submit(callable);
+			// add Future to the list,we can get return value using Future.
+			list.add(future);
+		}
+		for (Future<String> fut : list) {
+			try {
+				System.out.println(new Date() + "::" + fut.get());
+			} catch (InterruptedException | ExecutionException e) {
+				e.printStackTrace();
+			}
+		}
+		executor.shutdown();
+	}
+
+}
