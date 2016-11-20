5b523b19de6a94a991b7828572c3f396bb32b822
jinshui.yu <jinshui.yu@zhulebei.com>
2016-11-20 11:13:34 +0800

    判断文件类型方法

--- JavaTutorial/src/test/java/org/JavaTutorial/AppTest.java
+++ JavaTutorial/src/test/java/org/JavaTutorial/AppTest.java
@@ -1,5 +1,9 @@
 package org.JavaTutorial;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
 import org.junit.Test;
 
 /**
@@ -14,6 +18,33 @@ public class AppTest {
 			return;
 		}
 		System.out.println("Execute is ok");
+	}
+
+	private String prop = null;
+	@Test
+	public void test() throws InterruptedException {
+		for (int i = 0; i < 10; i++) {
+			getProperties("property.conf");
+			Thread.sleep(2000);
+			System.out.println(i + "当前参数值=" + prop);
+		}
+	}
+
+	private void getProperties(String fileName) {
+		InputStream inputStream = this.getClass().getClassLoader().getResourceAsStream(fileName);
+		Properties p = new Properties();
+		try {
+			p.load(inputStream);
+		} catch (IOException e1) {
+			System.out.println("配置文件读取失败，请检查.");
+			e1.printStackTrace();
+		}
+		setPropertiesValues(p);
+	}
+
+	private void setPropertiesValues(Properties p) {
+		prop = p.getProperty("HELLO");
+
 	}
 
 }
