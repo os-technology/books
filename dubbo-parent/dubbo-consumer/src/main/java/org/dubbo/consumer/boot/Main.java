87a859433806f1daa5e8fe3c0d32e55c05f9445a
jinshui.yu <jinshui.yu@zhulebei.com>
2017-01-29 18:12:37 +0800

    去掉多余pom内容，调整jdk参数配置名称

--- dubbo-parent/dubbo-consumer/src/main/java/org/dubbo/consumer/boot/Main.java
+++ dubbo-parent/dubbo-consumer/src/main/java/org/dubbo/consumer/boot/Main.java
@@ -8,12 +8,13 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Main {
 	public static void main(String[] args) {
-		ApplicationContext appConsumer = new ClassPathXmlApplicationContext("consumer.xml");
-		HelloService hello = (HelloService) appConsumer.getBean("hello");
-		String output = hello.hello("dubbo搞定。");
-		System.out.println(output);
-		System.out.println("consumer 启动成功。");
-		Scanner scanner = new Scanner(System.in);
-		scanner.nextLine();
+		//		ApplicationContext appConsumer = new ClassPathXmlApplicationContext("consumer.xml");
+//		HelloService hello = (HelloService) appConsumer.getBean("hello");
+//		String output = hello.hello("dubbo搞定。");
+//		System.out.println(output);
+//		System.out.println("consumer 启动成功。");
+//		Scanner scanner = new Scanner(System.in);
+//		scanner.nextLine();
+		com.alibaba.dubbo.container.Main.main(args);
 	}
 }
