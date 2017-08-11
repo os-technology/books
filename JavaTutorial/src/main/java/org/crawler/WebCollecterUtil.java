877692d45fe445cc9751d72ed75be3380237f8eb
jinshui.yu <jinshui.yu@zhulebei.com>
2017-08-11 10:17:18 +0800

    添加WebCollector，微信公众号爬取框架

---
+++ JavaTutorial/src/main/java/org/crawler/WebCollecterUtil.java
@@ -0,0 +1,13 @@
+package org.crawler;
+
+/**
+ * @author yuijnshui@lxfintech.com
+ * @Title: WebCollecterUtil
+ * @Copyright: Copyright (c) 2017
+ * @Description: <br>
+ * @Company: lxjr.com
+ * @Created on 2017/8/11上午10:06
+ */
+
+public class WebCollecterUtil {
+}
