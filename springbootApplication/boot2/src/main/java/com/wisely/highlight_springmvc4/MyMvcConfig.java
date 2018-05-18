895819583f0e401e2d1b10580432b029de437919
jinshui.yu <jinshui.yu@zhulebei.com>
2018-05-18 19:15:22 +0800

    添加boot2部分

---
+++ springbootApplication/boot2/src/main/java/com/wisely/highlight_springmvc4/MyMvcConfig.java
@@ -0,0 +1,18 @@
+package com.wisely.highlight_springmvc4;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author yuijnshui@lxfintech.com
+ * @Title: MyMvcConfig
+ * @Copyright: Copyright (c) 2017
+ * @Description: <br>
+ * @Company: lxjr.com
+ * @Created on 2018/5/18下午7:14
+ */
+@Configuration
+@ComponentScan
+public class MyMvcConfig {
+
+}
