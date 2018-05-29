d5fa8cff242f04931f3681f8c2e06dd56dba4a0d
jinshui.yu <jinshui.yu@zhulebei.com>
2018-05-29 17:46:07 +0800

    创建简单springboot启动模块，并修改启动时显示的banner图案信息

---
+++ springbootApplication/boot2/banner/src/main/java/com/packing/banner/boot/SpringBootApplicationBannerStart.java
@@ -0,0 +1,26 @@
+package com.packing.banner.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+/**
+ * @author yuijnshui@lxfintech.com
+ * @Title: SpringBootApplicationBannerStart
+ * @Copyright: Copyright (c) 2017
+ * @Description: <br>
+ * @Company: lxjr.com
+ * @Created on 2018/5/29下午4:27
+ */
+@SpringBootApplication
+@ComponentScan(basePackages = "com.packing.banner.boot")
+public class SpringBootApplicationBannerStart {
+
+    /**
+     * 注意：如果<springframework-version>4.2.1.RELEASE</springframework-version>版本为4.1.5.RELEASE，则无法正常启动，至少使用4.2.1.RELEASE以上版本方可。该pom文件配置在springbootApplication模块下。
+     * @param args
+     */
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootApplicationBannerStart.class, args);
+    }
+}
