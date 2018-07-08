7dc11da134112cb91ffcab54b4566639243147df
yujinshui <602570224@qq.com>
2018-07-08 11:07:39 +0800

    dubbo-consumer部分添加直连属性URL，保证注册中心挂掉时依旧可以通过缓存进行通讯

--- dubbo-parent/dubbo-consumer/src/test/java/org/dubbo/consumer/DubboConsumerTest.java
+++ dubbo-parent/dubbo-consumer/src/test/java/org/dubbo/consumer/DubboConsumerTest.java
@@ -1,5 +1,7 @@
 package org.dubbo.consumer;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Scanner;
 
 import org.dubbo.inter.api.HelloService;
@@ -12,13 +14,14 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
  * Unit test for simple App.
  */
 
-public class AppTest {
+public class DubboConsumerTest {
 
 	private static ApplicationContext appConsumer = new ClassPathXmlApplicationContext("consumer.xml");
 	public static void main(String[] args) {
+		String dateTime = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date());
 
 		HelloService hello = (HelloService) appConsumer.getBean(HelloService.class);
-		String output = hello.hello("dubbo搞定。");
+		String output = hello.hello("dubbo搞定。"+dateTime);
 		System.out.println(output);
 		System.out.println("consumer 启动成功。");
 //		Scanner scanner = new Scanner(System.in);
