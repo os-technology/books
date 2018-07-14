55e60eecf7f8f67e78918be9d72b29fa1defa84d
yujinshui <602570224@qq.com>
2018-07-14 21:24:23 +0800

    spring源码-2.1 容器基本用法

---
+++ Spring4/src/main/java/com/start/spring4/entity/ch2/MyTestBean.java
@@ -0,0 +1,23 @@
+package com.start.spring4.entity.ch2;
+
+/**
+ * @author yuijnshui
+ * @Title: MyTestBean
+ * @Copyright: Copyright (c) 2017
+ * @Description: <br>
+ * @Company: lxjr.com
+ * @Created on 2018/7/14下午9:17
+ */
+public class MyTestBean {
+
+    private String str ;
+
+    public String getStr() {
+        return str;
+    }
+
+    public MyTestBean setStr(String str) {
+        this.str = str;
+        return this;
+    }
+}
---
+++ Spring4/src/test/java/com/start/spring4/entity/ch2/BeanFactoryTest.java
@@ -0,0 +1,28 @@
+package com.start.spring4.entity.ch2;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.core.io.ClassPathResource;
+
+/**
+ * @author yuijnshui
+ * @Title: BeanFactoryTest
+ * @Copyright: Copyright (c) 2017
+ * @Description: <br>
+ * @Company: lxjr.com
+ * @Created on 2018/7/14下午9:19
+ */
+public class BeanFactoryTest {
+
+    @Test
+    public void testSimpleLoad(){
+        BeanFactory beanFactory = new XmlBeanFactory(new ClassPathResource("springmvc-beanFactoryTest.xml"));
+        MyTestBean myTestBean = (MyTestBean) beanFactory.getBean("myTestBean");
+
+        String eqlVal = "myTestBean";
+        myTestBean.setStr(eqlVal);
+        Assert.assertEquals(eqlVal,myTestBean.getStr());
+    }
+}
