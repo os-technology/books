9d38eb16c01a7b770636bfde4931ac403fff78cb
yujinshui <602570224@qq.com>
2018-07-15 19:32:37 +0800

    spring源码学习-2.1 容器基本配置-xml读取添加新的实现方式(配置文件路径也有所变化)

--- SpringSourceCode/src/test/java/com/spring/source/code/beans/ch2/BeanFactoryTest.java
+++ SpringSourceCode/src/test/java/com/spring/source/code/beans/ch2/BeanFactoryTest.java
@@ -3,8 +3,14 @@ package com.spring.source.code.beans.ch2;
 import org.junit.Assert;
 import org.junit.Test;
 import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.beans.factory.support.DefaultListableBeanFactory;
+import org.springframework.beans.factory.support.SimpleBeanDefinitionRegistry;
+import org.springframework.beans.factory.xml.XmlBeanDefinitionReader;
 import org.springframework.beans.factory.xml.XmlBeanFactory;
 import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.core.io.Resource;
 
 /**
  * @author yuijnshui
@@ -16,13 +22,42 @@ import org.springframework.core.io.ClassPathResource;
  */
 public class BeanFactoryTest {
 
+    /**
+     * XmlBeanFactory 过期。从spring 3.2 后推荐使用DefaultListableBeanFactory和XmlBeanDefinitionReader来代替它。
+     */
     @Test
-    public void testSimpleLoad(){
-        BeanFactory beanFactory = new XmlBeanFactory(new ClassPathResource("springmvc-beanFactoryTest.xml"));
+    public void testSimpleLoad_3_2_x_old() {
+        BeanFactory beanFactory = new XmlBeanFactory(new ClassPathResource("beanFactoryTest.xml"));
         MyTestBean myTestBean = (MyTestBean) beanFactory.getBean("myTestBean");
 
         String eqlVal = "myTestBean";
         myTestBean.setStr(eqlVal);
-        Assert.assertEquals(eqlVal,myTestBean.getStr());
+        Assert.assertEquals(eqlVal, myTestBean.getStr());
+        System.out.println(this);
     }
+
+    /**
+     * xml文件新的读取方式
+     *
+     * @param
+     * @return void
+     * @author code
+     * @date 2018/7/15 下午7:30
+     */
+    @Test
+    public void testSimpleLoad_new() {
+
+        DefaultListableBeanFactory factory = new DefaultListableBeanFactory();
+        Resource resource = new ClassPathResource("beanFactoryTest.xml", getClass());
+        new XmlBeanDefinitionReader(factory).loadBeanDefinitions(resource);
+
+        MyTestBean myTestBean = (MyTestBean) factory.getBean("myTestBean");
+
+        String eqlVal = "myTestBean";
+        myTestBean.setStr(eqlVal);
+        Assert.assertEquals(eqlVal, myTestBean.getStr());
+        System.out.println(this);
+    }
+
+
 }
