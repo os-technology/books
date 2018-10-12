fd7c00362c145c4131eba181c326e76eb24f15cf
yujinshui <602570224@qq.com>
2018-10-12 18:01:28 +0800

    controller单元测试新增Junit实现方式

--- JunitMockTest/src/test/java/com/code/junit/mock/boot/dict/controller/junit/MockControllerJunitTest.java
+++ JunitMockTest/src/test/java/com/code/junit/mock/boot/dict/controller/junit/MockControllerJunitTest.java
@@ -1,14 +1,11 @@
 package com.code.junit.mock.boot.dict.controller.junit;
 
 import com.alibaba.fastjson.JSON;
-import com.code.junit.mock.boot.app.JunitMockApplication;
 import com.code.junit.mock.boot.dict.beans.MockTable;
 import com.code.junit.mock.boot.dict.controller.MockController;
-import com.code.junit.mock.boot.dict.controller.MockControllerTest;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.ComponentScan;
@@ -18,7 +15,6 @@ import org.springframework.mock.web.MockHttpSession;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.AbstractTransactionalJUnit4SpringContextTests;
-import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.servlet.ModelAndView;
