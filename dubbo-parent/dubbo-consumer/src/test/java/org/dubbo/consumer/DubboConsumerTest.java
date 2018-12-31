864d76884291635a6c7e243d01031bcbfca99c15
yujinshui <602570224@qq.com>
2018-12-31 23:09:24 +0800

    dubbo服务，Java方式调用调试成功。2018 finish~

--- dubbo-parent/dubbo-consumer/src/test/java/org/dubbo/consumer/DubboConsumerTest.java
+++ dubbo-parent/dubbo-consumer/src/test/java/org/dubbo/consumer/DubboConsumerTest.java
@@ -4,7 +4,10 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Scanner;
 
+import com.alibaba.dubbo.config.ApplicationConfig;
+import com.alibaba.dubbo.config.RegistryConfig;
 import org.dubbo.inter.api.HelloService;
+import org.junit.Before;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
@@ -15,18 +18,53 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
  */
 
 public class DubboConsumerTest {
+    DubboConsumerConfig consumerConfig;
 
-	private static ApplicationContext appConsumer = new ClassPathXmlApplicationContext("consumer.xml");
-	public static void main(String[] args) {
-		String dateTime = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date());
+    DubboProxyFactory dubbo ;
+    @Before
+	public void init(){
+         consumerConfig = new DubboConsumerConfig();
 
-		HelloService hello = (HelloService) appConsumer.getBean(HelloService.class);
-		String output = hello.hello("dubbo搞定。"+dateTime);
-		System.out.println(output);
-		System.out.println("consumer 启动成功。");
+        // 当前应用配置
+        ApplicationConfig application = new ApplicationConfig();
+//        application.setName("payment-remote");
+        application.setName("dubbo_hello");
+
+        // 连接注册中心配置
+        RegistryConfig registry = new RegistryConfig();
+        registry.setProtocol("zookeeper");
+        //dev
+        String zookeeper_address_dev="zookeeper://127.0.0.1:2181?backup=127.0.0.1:2182,127.0.0.1:2183";
+        registry.setAddress(zookeeper_address_dev);
+
+         dubbo = new DubboProxyFactory(application,registry);
+
+
+
+    }
+
+    @Test
+    public void consumer(){
+
+//        HelloService helloService = (HelloService)dubbo.create(HelloService.class, "1.0.0");
+
+        HelloService helloService = consumerConfig.getConsumer(HelloService.class);
+        System.out.println("时间信息："+helloService.getToday());
+
+    }
+
+    private static ApplicationContext appConsumer = new ClassPathXmlApplicationContext("consumer.xml");
+
+    public static void main(String[] args) {
+        String dateTime = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date());
+
+        HelloService hello = (HelloService) appConsumer.getBean(HelloService.class);
+        String output = hello.hello("dubbo搞定。" + dateTime);
+        System.out.println(output);
+        System.out.println("consumer 启动成功。");
 //		Scanner scanner = new Scanner(System.in);
 //		scanner.nextLine();
-	}
+    }
 
 
 }
