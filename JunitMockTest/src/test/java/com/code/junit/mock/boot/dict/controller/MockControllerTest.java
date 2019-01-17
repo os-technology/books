2529c81b631296f23b535ab7230c7a776e990cf7
yujinshui <602570224@qq.com>
2019-01-17 13:56:32 +0800

    删除多余引用

--- JunitMockTest/src/test/java/com/code/junit/mock/boot/dict/controller/MockControllerTest.java
+++ JunitMockTest/src/test/java/com/code/junit/mock/boot/dict/controller/MockControllerTest.java
@@ -11,7 +11,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
