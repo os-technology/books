254b907c61786e5716aeb6a8c5c19ac40ad95f35
yujinshui <602570224@qq.com>
2019-02-15 14:02:46 +0800

    删除多余代码

--- dubbo-parent/dubbo-consumer/src/test/java/org/dubbo/consumer/DubboProxyFactory.java
+++ dubbo-parent/dubbo-consumer/src/test/java/org/dubbo/consumer/DubboProxyFactory.java
@@ -3,7 +3,6 @@ package org.dubbo.consumer;
 import com.alibaba.dubbo.config.ApplicationConfig;
 import com.alibaba.dubbo.config.ReferenceConfig;
 import com.alibaba.dubbo.config.RegistryConfig;
-import org.springframework.util.StringUtils;
 
 /**
  * @author code
@@ -26,7 +25,7 @@ public class DubboProxyFactory {
         reference = new ReferenceConfig(); // 此实例很重，封装了与注册中心的连接以及与提供者的连接，请自行缓存，否则可能造成内存和连接泄漏
         reference.setApplication(application);
         reference.setRegistry(registry); // 多个注册中心可以用setRegistries()
-        if(timeout != null) {
+        if (timeout != null) {
             reference.setTimeout(timeout);
         }
     }
@@ -35,13 +34,16 @@ public class DubboProxyFactory {
         // 注意：ReferenceConfig为重对象，内部封装了与注册中心的连接，以及与服务提供方的连接
         // 引用远程服务
         reference.setInterface(clazz);
-        if(!StringUtils.isEmpty(version)) {
+        if (isNotEmpty(version)) {
             reference.setVersion(version);
         }
 
         return reference.get();
     }
 
+    private boolean isNotEmpty(Object str) {
+        return !(str == null || "".equals(str));
+    }
 
     public Integer getTimeout() {
         return timeout;
