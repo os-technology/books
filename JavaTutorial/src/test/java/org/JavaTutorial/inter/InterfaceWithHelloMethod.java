cec3dcbac14b8ac3057d6dd44eb580d2fa765f59
yujinshui <602570224@qq.com>
2019-03-25 14:49:21 +0800

    两个不同的接口实现相同的方法

---
+++ JavaTutorial/src/test/java/org/JavaTutorial/inter/InterfaceWithHelloMethod.java
@@ -0,0 +1,13 @@
+package org.JavaTutorial.inter;
+
+/**
+ * @author code
+ * @Title: InterfaceWithHelloMethod
+ * @Copyright: Copyright (c) 2017
+ * @Description: <br>
+ * @Company: www.qdingnet.com
+ * @Created on 2019/3/25下午2:46
+ */
+public interface InterfaceWithHelloMethod {
+    String hello(String input);
+}
---
+++ JavaTutorial/src/test/java/org/JavaTutorial/inter/InterfaceWithHelloMethodImpl.java
@@ -0,0 +1,16 @@
+package org.JavaTutorial.inter;
+
+/**
+ * @author code
+ * @Title: InterfaceWithHelloMethodImpl
+ * @Copyright: Copyright (c) 2017
+ * @Description: <br>
+ * @Company: www.qdingnet.com
+ * @Created on 2019/3/25下午2:47
+ */
+public class InterfaceWithHelloMethodImpl implements InterfaceWithHelloMethod,InterfaceWithHelloMethod2 {
+    @Override
+    public String hello(String input) {
+        return null;
+    }
+}
