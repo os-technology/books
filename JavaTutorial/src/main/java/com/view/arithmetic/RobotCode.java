477dafc6023aff1644659f9a1cd742b7e37acc37
yujinshui <602570224@qq.com>
2020-01-08 19:29:40 +0800

    机器人编号问题解决

--- JavaTutorial/src/main/java/com/view/arithmetic/RobotCode.java
+++ JavaTutorial/src/main/java/com/view/arithmetic/RobotCode.java
@@ -10,6 +10,7 @@ import java.util.regex.Pattern;
  * <p>
  * 一批机器人，按照 AA001  AA002...AA999,BA001 ....ZZ999，进行编号。
  * 要求每次编号的起始位置为上一个编号为基础，整体为连续性编号。
+ * TODO  存在问题
  *
  * @author code
  * @Title: RobotCode
@@ -25,7 +26,6 @@ public class RobotCode {
         if (code.charAt(1) != 'Z' || !code.contains("Z1000")) {
             return code.charAt(0) + "";
         }
-
         return String.valueOf((char) (code.charAt(0) + 1));
 
     }
@@ -36,12 +36,15 @@ public class RobotCode {
 
         Matcher matcher = pattern.matcher(code);
         Integer num = Integer.valueOf(matcher.replaceAll(""));
-        if (num < 999) {
+        if (num < 999 && num != 0) {
             return String.valueOf(code.charAt(1));
         }
         if (code.charAt(1) == 'Z') {
             return "A";
         }
+//        else if (code.contains("ZZ")) {//超过范围，直接返回 -1
+//            return "-1";
+//        }
         return String.valueOf((char) (code.charAt(1) + 1));
     }
 
@@ -56,7 +59,7 @@ public class RobotCode {
             return getThreeNum(val);
         }
 
-        return "001";
+        return "000";
     }
 
     /**
@@ -75,36 +78,38 @@ public class RobotCode {
      * @param count 机器人数量
      * @return
      */
-    private List<String> getCode(String code, int count) {
+    private List<String> getCode(String code, int count) throws Exception {
         List<String> codeList = new ArrayList<>();
         String a = first(code);
         String b = second(code);
         String c = num(code);
+        String cnum = c;
 
 
         for (int i = 0; i < count; i++) {
-            int numcode = Integer.valueOf(c);
-            int tmpNum = numcode + i % 1000;//取余保证数字部分在1-999之间变化
+            int numcode = Integer.valueOf(cnum);
+            int tmpNum = (numcode + i) % 1000;//取余保证数字部分在1-999之间变化
             String tmpCode = a + b + getThreeNum(String.valueOf(tmpNum));
             if (tmpNum % 100 == 0) {//内容输出格式化
                 codeList.add("\n");
             }
-            if (tmpNum >= 1000) {
+            if (tmpNum == 0) {
                 a = first(tmpCode);
                 b = second(tmpCode);
-                c = "001";
+                if ("-1".equals(b)) {
+                    throw new Exception("机器人数量超过编号范围,i=" + i);
+                }
+                c = "000";
                 tmpCode = a + b + getThreeNum(String.valueOf(c));
 
-
             }
-
             codeList.add(tmpCode);
         }
         return codeList;
     }
 
 
-    private void print(String code, int count) {
+    private void print(String code, int count) throws Exception {
         List<String> result = getCode(code, count);
         for (String val : result) {
             System.out.print(val + " ");
@@ -112,11 +117,12 @@ public class RobotCode {
         System.out.println();
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         RobotCode rc = new RobotCode();
         rc.print("AB991", 9);
         rc.print("AZ991", 9);
         rc.print("AK991", 1159);
+        rc.print("ZY991", 100);
 
 
     }
